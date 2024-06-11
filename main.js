91573295566ab4140a5ded19c8022afb4c079379
HriqueP <hapoletti@gmail.com>
2024-06-11 14:48:26 -0300

    connection with API

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-console.log("Hello Word");
 // https://api.quotable.io/random - Random Quote
 // https://api.quotable.io/quotes/random - Random Quotes
+
+fetch("https://api.quotable.io/random").then((response) => {
+  if (response.ok) {
+    response.json().then((data) => {
+      console.log(data);
+    });
+  } else {
+    console.log("Some problem with URL");
+  }
+});
