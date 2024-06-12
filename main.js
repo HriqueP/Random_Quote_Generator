4256ca1603e1d9fa5db9cf2faffcdfa8b6468c3b
HriqueP <hapoletti@gmail.com>
2024-06-12 10:57:57 -0300

    phrase and author on card

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+const phrase = document.querySelector(".phrase");
+const author = document.querySelector(".author");
+
 // https://api.quotable.io/random - Random Quote
 // https://api.quotable.io/quotes/random - Random Quotes
 
 fetch("https://api.quotable.io/random").then((response) => {
   if (response.ok) {
     response.json().then((data) => {
-      console.log(data);
+      const content = `
+        <img src="./assets/open-quotes.png" width="20" />
+        ${data.content}
+        <img src="./assets/close-quotes.png" width="20" />
+      `;
+      phrase.innerHTML = content;
+      author.innerText = "- " + data.author;
     });
   } else {
     console.log("Some problem with URL");
