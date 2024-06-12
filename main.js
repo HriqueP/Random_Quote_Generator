104cc14278f3562a6135132e5823329fdf2f3473
HriqueP <hapoletti@gmail.com>
2024-06-12 19:13:23 -0300

    async await function

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-const phrase = document.querySelector(".phrase");
-const author = document.querySelector(".author");
-
 // https://api.quotable.io/random - Random Quote
 // https://api.quotable.io/quotes/random - Random Quotes
 
-fetch("https://api.quotable.io/random").then((response) => {
-  if (response.ok) {
-    response.json().then((data) => {
-      const content = `
-        <img src="./assets/open-quotes.png" width="20" />
-        ${data.content}
-        <img src="./assets/close-quotes.png" width="20" />
-      `;
-      phrase.innerHTML = content;
-      author.innerText = "- " + data.author;
-    });
-  } else {
-    console.log("Some problem with URL");
-  }
-});
+// Page Elements
+const phrase = document.querySelector(".phrase");
+const author = document.querySelector(".author");
+
+// Refresh quote on button click
+async function showQuote() {
+  const response = await fetch("https://api.quotable.io/random");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+
+  const content = ` 
+    <img src="./assets/open-quotes.png" width="20" />
+    ${data.content}
+    <img src="./assets/close-quotes.png" width="20" />`;
+
+  phrase.innerHTML = content;
+  author.innerText = "- " + data.author;
+}
+
+// Quote when page first loads
+showQuote();
